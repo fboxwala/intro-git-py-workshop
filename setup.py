373aac50d08b8eeeef1b8e80af7a556d41433b9c
Fatema Boxwala <fbox@yelp.com>
2017-10-13 16:01:51 -0700

    adds a setup.py to build our package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='caduceus',
+    version='0.1',
+    description='A simple tool to compare snake speeds',
+    author='fbox',
+    author_email='',
+    url='',
+    license='GNU GPL v3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+)

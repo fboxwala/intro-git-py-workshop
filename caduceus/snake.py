24864eaf71b6b8394b2ac95ebe831ea8c1c94e04
Richard Saethang <richard.saethang@gmail.com>
2017-10-18 20:18:02 -0700

    add docstrings everywhere, resolves #1

--- caduceus/snake.py
+++ caduceus/snake.py
@@ -1,5 +1,6 @@
 class Snake:
     def __init__(self, length, weight, common_name, sci_name):
+    	'''Initializer for the Snake class'''
         self.length = length
         self.weight = weight
         self.common_name = common_name
--- caduceus/sorter.py
+++ caduceus/sorter.py
@@ -1,10 +1,13 @@
 class SnakeSorter:
     def __init__(self, snakes):
+    	'''Initializer for the SnakeSorter class'''
         self.snakes = snakes
         self.sorted_snakes = []
 
     def sort_by_weight(self):
+    	'''Sort snakes by weight'''
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.weight)
 
     def sort_by_length(self):
+    	'''Sort snakes by length'''
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.length)

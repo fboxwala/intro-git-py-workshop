4bdba097833d56bcb3f372812fdb026dac9a841a
Hildeberto <hildeberto@gmail.com>
2017-12-17 18:12:24 -0300

    fixes #3, error handling at cvs files structure

--- caduceus/core.py
+++ caduceus/core.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 from .snake import Snake
 from .sorter import SnakeSorter
 
@@ -7,11 +8,14 @@ def build_snakes(common_file, sci_file):
     Reads in the data stored in two files and builds a list of snakes
     
     Args:
-        common_file: a csv file mapping common names to scientific 
-        names of snakes
-        sci_file: a csv file listing scientific names of snakes with 
-        lengths and weights 
+        common_file: a csv file mapping common names to scientific names of snakes
+
+        common_file structure: common name, scientific name
+
+        sci_file: a csv file listing scientific names of snakes with lengths and weights 
     
+        sci_file structure: scientific name, weight, length
+
     Returns: an unsorted list of Snake items
     '''
     snakes = []
@@ -19,16 +23,24 @@ def build_snakes(common_file, sci_file):
     with open(common_file, 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            s = Snake(0, 0, row["common name"], row["scientific name"]) 
-            snakes.append(s)
-    
+            try:
+                s = Snake(0, 0, row["common name"], row["scientific name"]) 
+                snakes.append(s)
+            except (TypeError, ValueError):
+                print('Caduceus expects specific columns in csv files to work. See help(build_snakes) for the specifications')
+                sys.exit()
+
     for snake in snakes:
         with open(sci_file, 'rb') as csvfile2:
             reader2 = csv.DictReader(csvfile2)
             for row in reader2:
                 if snake.sci_name == row['scientific name']:
-                    snake.weight = int(row["weight"])
-                    snake.length = int(row['length'])
+                    try:
+                        snake.weight = int(row["weight"])
+                        snake.length = int(row['length'])
+                    except (TypeError, ValueError):
+                        print('Caduceus expects specific columns in csv files to work. See help(build_snakes) for the specifications')
+                        sys.exit()
 
     return snakes
 

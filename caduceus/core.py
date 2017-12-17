7e15f57f6d00583a1dd316242f0475e68d474c21
Hildeberto <hildeberto@gmail.com>
2017-12-17 18:53:14 -0300

    fixes #5, add support for parsing data about birds

--- caduceus/core.py
+++ caduceus/core.py
@@ -1,7 +1,8 @@
 import csv
 import sys
+from .bird import Bird
 from .snake import Snake
-from .sorter import SnakeSorter
+from .sorter import SnakeSorter, BirdSorter
 
 def build_snakes(common_file, sci_file):
     '''
@@ -24,7 +25,7 @@ def build_snakes(common_file, sci_file):
         reader = csv.DictReader(csvfile)
         for row in reader:
             try:
-                s = Snake(0, 0, row["common name"], row["scientific name"]) 
+                s = Snake(0, 0, row['common name'], row['scientific name']) 
                 snakes.append(s)
             except (TypeError, ValueError):
                 print('Caduceus expects specific columns in csv files to work. See help(build_snakes) for the specifications')
@@ -36,7 +37,7 @@ def build_snakes(common_file, sci_file):
             for row in reader2:
                 if snake.sci_name == row['scientific name']:
                     try:
-                        snake.weight = int(row["weight"])
+                        snake.weight = int(row['weight'])
                         snake.length = int(row['length'])
                     except (TypeError, ValueError):
                         print('Caduceus expects specific columns in csv files to work. See help(build_snakes) for the specifications')
@@ -55,3 +56,58 @@ def print_snakes_by_weight(snakes):
     
     for snek in sorterer.sorted_snakes:
         print('{name}: {wt}g'.format(name=snek.common_name, wt=snek.weight)) 
+
+
+def build_birds(common_file, sci_file):
+    '''
+    Reads in the data stored in two files and builds a list of birds
+    
+    Args:
+        common_file: a csv file mapping common names to scientific names of birds
+
+        common_file structure: common name, scientific name
+
+        sci_file: a csv file listing scientific names of birds with weights and wingspans
+    
+        sci_file structure: scientific name, weight, wingspan
+
+    Returns: an unsorted list of Bird items
+    '''
+    birds = []
+
+    with open(common_file, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            try:
+                b = Bird(0, 0, row['common name'], row['scientific name']) 
+                birds.append(b)
+            except (TypeError, ValueError):
+                print('Caduceus expects specific columns in csv files to work. See help(build_birds) for the specifications')
+                sys.exit()
+
+    for bird in birds:
+        with open(sci_file, 'rb') as csvfile2:
+            reader2 = csv.DictReader(csvfile2)
+            for row in reader2:
+                if bird.sci_name == row['scientific name']:
+                    try:
+                        bird.weight = int(row['weight'])
+                        bird.wingspan = int(row['wingspan'])
+                    except (TypeError, ValueError):
+                        print('Caduceus expects specific columns in csv files to work. See help(build_birds) for the specifications')
+                        sys.exit()
+
+    return birds
+
+def print_birds_by_wingspan(birds):
+    '''
+    Prints common names of birds, one per line, sorted by wingspan
+    
+    Args:
+        birds: a list of Bird objects
+    '''
+    sorterer = BirdSorter(birds)
+    sorterer.sort_by_wingspan()
+    
+    for bird in sorterer.sorted_birds:
+        print('{name}: {ws}cm'.format(name=bird.common_name, ws=bird.wingspan)) 
--- caduceus/sorter.py
+++ caduceus/sorter.py
@@ -8,3 +8,15 @@ class SnakeSorter:
 
     def sort_by_length(self):
         self.sorted_snakes = sorted(self.snakes, key=lambda snek: snek.length)
+
+
+class BirdSorter:
+    def __init__(self, birds):
+        self.birds = birds
+        self.sorted_birds = []
+
+    def sort_by_weight(self):
+        self.sorted_birds = sorted(self.birds, key=lambda bird: bird.weight)
+
+    def sort_by_wingspan(self):
+        self.sorted_birds = sorted(self.birds, key=lambda bird: bird.wingspan)

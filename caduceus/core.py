392ad77b42c5d063a63424a8fefeb86a771f384f
Hannah <hhannahwongg@gmail.com>
2018-03-15 19:33:57 -0400

    always be using single quotes

--- caduceus/core.py
+++ caduceus/core.py
@@ -5,13 +5,13 @@ from .sorter import SnakeSorter
 def build_snakes(common_file, sci_file):
     '''
     Reads in the data stored in two files and builds a list of snakes
-    
+
     Args:
-        common_file: a csv file mapping common names to scientific 
+        common_file: a csv file mapping common names to scientific
         names of snakes
-        sci_file: a csv file listing scientific names of snakes with 
-        lengths and weights 
-    
+        sci_file: a csv file listing scientific names of snakes with
+        lengths and weights
+
     Returns: an unsorted list of Snake items
     '''
     snakes = []
@@ -19,15 +19,15 @@ def build_snakes(common_file, sci_file):
     with open(common_file, 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            s = Snake(0, 0, row["common name"], row["scientific name"]) 
+            s = Snake(0, 0, row['common name'], row['scientific name'])
             snakes.append(s)
-    
+
     for snake in snakes:
         with open(sci_file, 'rb') as csvfile2:
             reader2 = csv.DictReader(csvfile2)
             for row in reader2:
                 if snake.sci_name == row['scientific name']:
-                    snake.weight = int(row["weight"])
+                    snake.weight = int(row['weight'])
                     snake.length = int(row['length'])
 
     return snakes
@@ -40,6 +40,6 @@ def print_snakes_by_weight(snakes):
     '''
     sorterer = SnakeSorter(snakes)
     sorterer.sort_by_weight()
-    
+
     for snek in sorterer.sorted_snakes:
-        print('{name}: {wt}g'.format(name=snek.common_name, wt=snek.weight)) 
+        print('{name}: {wt}g'.format(name=snek.common_name, wt=snek.weight))

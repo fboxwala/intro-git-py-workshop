2d0f6c9e7845b0eb512ca3bc5f26a0b42ad3b577
Yitong Xiao <alexiao@foxmail.com>
2018-03-15 19:51:42 -0400

    Added docstrings, solves issue 1

--- caduceus/snake.py
+++ caduceus/snake.py
@@ -1,5 +1,7 @@
 class Snake:
+	
     def __init__(self, length, weight, common_name, sci_name):
+	'''intialize a  snake'''
         self.length = length
         self.weight = weight
         self.common_name = common_name
